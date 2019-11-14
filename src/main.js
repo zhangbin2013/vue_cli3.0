6bffad97a1e891009e31670be6cf1a701925a111
zhangbin_yq <zhangbin_yq@kedacom.com>
2019-11-14 16:44:01 +0800

    修改引入版本

--- src/main.js
+++ src/main.js
@@ -2,11 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// import phoneVue from 'phone-pay-v'
 
 Vue.config.productionTip = false
 // 是否为测试环境
 Vue.prototype.$istest = process.env.VUE_APP_CURRENTMODE === 'test'
 
+// Vue.use(phoneVue)
 console.log('当前NODE_ENV：' + process.env.NODE_ENV)
 console.log('当前VUE_APP_CURRENTMODE：' + process.env.VUE_APP_CURRENTMODE)
 console.log('是否为测试环境：' + Vue.prototype.$istest)
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,51 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <div @click="showPayPanel">支付</div>
+    <!-- <vue-pay-pops :payPanelOptions="payPanelOptions" @inputOk="inputOk"></vue-pay-pops> -->
+    <input type="text" v-model="pwd">
+    <!-- <mt-switch v-model="value" @change="change"></mt-switch> -->
+    <phone-pay :payPanelOptions="payPanelOptions" @inputOk="inputOk"></phone-pay>
   </div>
 </template>
+<script>
+import axios from 'axios'
+import { vuePayPops } from 'phone-pay-v/'
+
+export default {
+  name: 'about',
+  components: {
+    PhonePay: vuePayPops
+  },
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      pwd: '',
+      value: false,
+      payPanelOptions: {
+        isShow: false,
+        pressKeyBackgorund: 'pink',
+        pwdLength: 6
+      }
+    }
+  },
+  methods: {
+    showPayPanel () {
+      this.payPanelOptions.isShow = true
+    },
+    inputOk (res) {
+      this.pwd = res
+      axios.get(`http://94.191.49.231:3000/pwdtest?pwd=${res}`).then(res => {
+        this.payPanelOptions.isShow = false
+        if (res.data.result.msg === 'success') {
+          // ok
+          alert('验证通过')
+        } else {
+          // false
+          alert('验证失败')
+        }
+      })
+    }
+  }
+}
+</script>
