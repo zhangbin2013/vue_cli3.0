80cbe54865ce97c9420d8140f21e4779f2642eac
zhangbin_yq <zhangbin_yq@kedacom.com>
2019-11-14 14:42:10 +0800

    update vue config last

--- src/main.js
+++ src/main.js
@@ -4,6 +4,12 @@ import router from './router'
 import store from './store'
 
 Vue.config.productionTip = false
+// 是否为测试环境
+Vue.prototype.$istest = process.env.VUE_APP_CURRENTMODE === 'test'
+
+console.log('当前NODE_ENV：' + process.env.NODE_ENV)
+console.log('当前VUE_APP_CURRENTMODE：' + process.env.VUE_APP_CURRENTMODE)
+console.log('是否为测试环境：' + Vue.prototype.$istest)
 
 new Vue({
   router,
